b0a741f3f26f6148dc9d64be2959f35136fbd8da
daniilro <daniil_ro@mail.ru>
2021-06-09 18:08:22 +0300

    dev

--- Урок 8. Практическое задание/make_server.bat
+++ Урок 8. Практическое задание/make_server.bat
@@ -1,3 +1,5 @@
+(
 cd server
 rem python setup.py sdist bdist_wheel
 python setup_server.py build_exe
+)
--- Урок 8. Практическое задание/server/setup_server.py
+++ Урок 8. Практическое задание/server/setup_server.py
@@ -1,6 +1,18 @@
 '''
+server_setup.py
+'''
 
 '''
+c)	cx_freeze некорректно обрабатывает SQLAlchemy, поэтому после подготовки дистрибутива нужно вручную заменить папку sqlalchemy в папке lib на аналогичную папку из директории интерпретатора.
+d)	Перейдите в директорию lib -> PyQt5 -> Qt -> plugins. Нужно вырезать папку platforms и перенести в директорию, где находится exe-файл.
+
+"No module named 'encodings" починилось после
+pip install git+https://github.com/anthony-tuininga/cx_Freeze.git
+
+https://coderoad.ru/28258991/%D0%9E%D1%88%D0%B8%D0%B1%D0%BA%D0%B0-cx_Freeze-Fatal-Python-Py_Initialize-%D0%BD%D0%B5-%D1%83%D0%B4%D0%B0%D0%BB%D0%BE%D1%81%D1%8C-%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C-%D0%BA%D0%BE%D0%B4%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D1%83
+
+'''
+
 import sys
 from cx_Freeze import setup, Executable
 
@@ -15,7 +27,7 @@ setup(
         "build_exe": build_exe_options
     },
     executables=[Executable('server.py',
-                            #base='Win32GUI',
+                            # base='Win32GUI',
                             targetName='server.exe',
                             )]
 )
