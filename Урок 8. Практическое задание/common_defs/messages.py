9df5617ac8f253c7141a50a4b5c3b4f6c4021425
daniilro <daniil_ro@mail.ru>
2021-06-09 14:13:09 +0300

    dev

---
+++ Урок 8. Практическое задание/common_defs/messages.py
@@ -0,0 +1,36 @@
+"""
+
+
+"""
+
+import json
+
+from common_defs import DATA_PACK_SIZE, DEFAULT_ENCODING
+from errors import IncorrectDataRecivedError, NonDictInputError
+
+
+######################################################################
+# @log
+def get_message(client):
+    encoded_response = client.recv(DATA_PACK_SIZE)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(DEFAULT_ENCODING)
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        else:
+            raise IncorrectDataRecivedError
+    else:
+        raise IncorrectDataRecivedError
+
+
+######################################################################
+# @log
+def send_message(sock, message):
+    if not isinstance(message, dict):
+        raise NonDictInputError
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(DEFAULT_ENCODING)
+    sock.send(encoded_message)
+
+######################################################################
